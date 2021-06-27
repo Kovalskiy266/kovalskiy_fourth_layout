eed07a51752da41aca626c18f5d45a4d205698dd
Kovalskiy <kovalevskiiy16@ukr.net>
2021-06-27 20:07:21 +0300

    made a second slider

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Другий слайдер
+$(document).ready(function(){
+    $('.second_slider').slick({
+        arrows: false,
+        dots: true,
+        // autoplay: true,
+        infinite: true, 
+        // autoplaySpeed: 4000,
+        // pauseOnHover: false,
+        // fade: true,
+        // draggable: false,
+        // pauseOnFocus: false,
+        // pauseOnDotsHover: false,
+        // fade: true, 
+        // waitForAnimate: false
+    });
+});
