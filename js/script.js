4b5a34ef3dac0b7e9a8ef2a7de97a0e91be57158
Kovalskiy <kovalevskiiy16@ukr.net>
2021-06-30 18:35:32 +0300

    adaptive in process

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 //Другий слайдер
+
+// var screen = window.matchMedia("(max-width:850px)");
+// console.log(screen)
+
+
 $(document).ready(function(){
+
     $('.second_slider').slick({
         arrows: false,
         dots: true,
@@ -7,38 +13,80 @@ $(document).ready(function(){
         infinite: true, 
         autoplaySpeed: 10000,
         // pauseOnHover: false,
-        fade: true,
+        fade: true
         // draggable: false,
         // pauseOnFocus: false,
         // pauseOnDotsHover: false,
         // fade: true, 
         // waitForAnimate: false
     });
+
     $('.thirth_slider').slick({
         arrows: false,
         dots: true,
         autoplay: true,
         infinite: true, 
-        autoplaySpeed: 10000,
+        autoplaySpeed: 10000
         
     });
-    // $('.first_slider').slick({
-    //     arrows: false,
-    //     dots: false,
-    //     infinite: true,
-      
+
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+    });
+    // $('.menu-burger__header').click(function() {
+    //     $('.menu-burger__header').toggleClass('open-menu');
     // });
 
-    // if (window.matchMedia("(max-width: 750px)").matches) {
-    //     $('.first_slider').slick({
-    //         arrows: false, 
-    //         dots: true,
-    //         infinite: true,
-    //         autoplay: true
-    //     })
-    // } 
+    $('.first_slider').slick({
+        
+        // autoplay:true,
+        arrows: false,
+        dots: false,
+        // speed: 300,
+        infinite: true,
+        slidesToShow: 3,
+        responsive:[
+            {
+                breakpoint: 851,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow:2,
+                    infinite: true,
+                    slidesToScroll: 1
+                }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                dots: true,
+                arrows: false,
+                slidesToShow:1,
+                infinite: true,
+                slidesToScroll: 1
+            }
+        }
 
+
+        ]
+        })
 });
+
+
+
+
+ // $('.gamburger').click(function(){
+    //     $('.menu').toggleClass('menu-active');
+    // });
+
+    // $('.menu a').click(function() {
+
+    //     $('.menu').removeClass('menu-active');
+    
+    // });
+
 // var sld = function() {
 //     if ($(window).width() > 320) {
 //     $('.slider').bxSlider({
